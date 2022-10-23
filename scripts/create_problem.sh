5fd5f69ffa39797a02188cbaf54100ec3a243a42
Jiahui Chen <chenjiahui1991@163.com>
2022-10-23 10:59:14 +0800

    [FEAT] setup utils and add P1000

---
+++ scripts/create_problem.sh
@@ -0,0 +1,5 @@
+base_dir=problems/$1/
+mkdir -p ${base_dir}
+touch ${base_dir}description.md
+touch ${base_dir}solution.md
+touch ${base_dir}solution.cpp
---
+++ scripts/run.sh
@@ -0,0 +1,4 @@
+set -e
+base_dir=problems/$1/
+g++ ${base_dir}solution.cpp --std=c++20 -O2 && ./a.out
+rm a.out
