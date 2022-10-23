3cd18ca272fa53d4e6b038582474024cbfeef8cb
Jiahui Chen <chenjiahui1991@163.com>
2022-10-23 17:38:51 +0800

    [FEAT] add P1001

--- scripts/create_problem.sh
+++ scripts/create_problem.sh
@@ -3,3 +3,20 @@ mkdir -p ${base_dir}
 touch ${base_dir}description.md
 touch ${base_dir}solution.md
 touch ${base_dir}solution.cpp
+
+cat >> ${base_dir}solution.cpp <<EOF
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
+EOF
+
+cat >> ${base_dir}solution.md <<EOF
+# 题解及思考
+EOF
